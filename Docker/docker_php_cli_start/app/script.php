fe8b2304363fedf6fbeedd2c95ae487d0e563587
lyucean <lyucean@gmail.com>
2023-06-14 14:14:23 +0300

    перенёс расположение .env

--- Docker/docker_php_cli_start/app/script.php
+++ Docker/docker_php_cli_start/app/script.php
@@ -2,4 +2,15 @@
 
 require_once('vendor/autoload.php');
 
-echo 'Hello world' . PHP_EOL;
+// use the factory to create a Faker\Generator instance
+//$faker = Faker\Factory::create();
+
+// generate data by accessing properties
+
+
+//echo "Hello, World! " . $faker->name . PHP_EOL;
+
+while (true) {
+    echo "Current date and time: " . date("Y-m-d H:i:s") . PHP_EOL;
+    sleep(1); // Задержка 1 секунда перед каждой итерацией цикла
+}
